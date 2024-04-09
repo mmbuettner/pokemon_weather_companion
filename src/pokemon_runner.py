af61dfc114073e11c2be8e2e78ab28a52a7bec37
Meike Buettner <mbuettn7@ford.com>
2024-04-08 21:10:26 -0500

    undid formatting

--- src/pokemon_runner.py
+++ src/pokemon_runner.py
@@ -1,9 +1,7 @@
 from collections import OrderedDict
 
 from weather_runner import weather_dict
-from helper_functions import (
-    db_select_query
-)
+from helper_functions import db_select_query
 
 pogo_weather_companion_json = {}
 pogo_weather_companion_json = weather_dict()
@@ -19,9 +17,7 @@ def boosted_types():
     for boost in boosts:
         boost_list.append(boost[0])
 
-    boosted_type_dict = {
-        'boosted_types' : boost_list
-    }
+    boosted_type_dict = {"boosted_types": boost_list}
     pogo_weather_companion_json.update(boosted_type_dict)
 
     return boost_list
@@ -30,9 +26,7 @@ def boosted_types():
 def double_damage_to_types(boost_list):
     double_damage_to_dict = {}
     for boost in boost_list:
-        boost_dict = {
-            boost : ''
-        }
+        boost_dict = {boost: ""}
         double_damage_to_dict.update(boost_dict)
 
     type_query = f"type LIKE '{boost_list[0]}'"
@@ -64,15 +58,11 @@ def double_damage_to_types(boost_list):
         for opponent in double_damage_to:
             print(opponent[0])
             if opponent[0] == key:
-                opponent_dict = {
-                    'opponent' : opponent[1]
-                }
+                opponent_dict = {"opponent": opponent[1]}
                 opponent_list.append(opponent_dict)
         double_damage_to_dict[key] = opponent_list
 
-    full_double_damage_dict = {
-        'double_damage_to' : [double_damage_to_dict]
-    }
+    full_double_damage_dict = {"double_damage_to": [double_damage_to_dict]}
     pogo_weather_companion_json.update(full_double_damage_dict)
 
     return full_double_damage_dict
@@ -80,22 +70,20 @@ def double_damage_to_types(boost_list):
 
 def pokemon_with_boosted_types():
 
-    boost_list = ['fighting', 'poison', 'fairy']
+    boost_list = ["fighting", "poison", "fairy"]
 
     double_damage_to_types(boost_list)
 
     pokemon_with_boost_dict = {}
     for boost in boost_list:
-        boost_dict = {
-            boost : ''
-        }
+        boost_dict = {boost: ""}
         pokemon_with_boost_dict.update(boost_dict)
 
     type_query = f"type LIKE '{boost_list[0]}'"
     for i in range(1, len(boost_list)):
         or_query = f" OR type LIKE '{boost_list[i]}'"
         type_query = type_query + or_query
-    
+
     sql_stmt = f"""
         SELECT * FROM public.pokemon_by_type WHERE ({type_query})
     """
@@ -104,16 +92,11 @@ def pokemon_with_boosted_types():
         pokemon_list = []
         for pokemon in boosted_pokemon:
             if pokemon[1] == key:
-                pokemon_dict = {
-                    "pokemon" : pokemon[0],
-                    'sprite' : pokemon[2]
-                }
+                pokemon_dict = {"pokemon": pokemon[0], "sprite": pokemon[2]}
                 pokemon_list.append(pokemon_dict)
         pokemon_with_boost_dict[key] = pokemon_list
 
-    full_boost_dict = {
-        "pokemon_with_boost" : [pokemon_with_boost_dict]
-    }
+    full_boost_dict = {"pokemon_with_boost": [pokemon_with_boost_dict]}
 
     pogo_weather_companion_json.update(full_boost_dict)
     print(pogo_weather_companion_json)
@@ -125,5 +108,5 @@ def main():
     pokemon_with_boosted_types()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
